09535e120ffab8e080df685e98ee6f02c3fcdb93
Nick <45113563+NightWolf1038@users.noreply.github.com>
2020-02-08 16:52:34 -0300

    1.1
    Added background color and hour color

--- ClockPluginRedux/ClockPluginRedux.plugin.js
+++ ClockPluginRedux/ClockPluginRedux.plugin.js
@@ -1,7 +1,7 @@
 //META{"name":"ClockPluginRedux","displayName":"ClockPluginRedux","website":"https://github.com/NightWolf1038/BetterDiscord-Plugins/tree/master/ClockPluginRedux/","source":"https://raw.githubusercontent.com/NightWolf1038/BetterDiscord-Plugins/master/ClockPluginRedux/ClockPluginRedux.plugin.js"}*//
 
 var ClockPluginRedux = (() => {
-	const config = {"main":"index.js","info":{"name":"ClockPluginRedux","authors":[{"name":"NightWolf","discord_id":"242126810026868738","github_username":"NightWolf1038"}],"version":"1.0","description":"Adds a clock to Discord. Based on Jiiks' plugin","github":"https://github.com/NightWolf1038/BetterDiscord-Plugins/tree/master/ClockPluginRedux/","github_raw":"https://raw.githubusercontent.com/NightWolf1038/BetterDiscord-Plugins/master/ClockPluginRedux/ClockPluginRedux.plugin.js"},"changelog":[{"title":"What's New?","items":["Initial release"]}],"defaultConfig":[{"type":"switch","id":"format","name":"24 Hour Format","note":"Change the clock format.","value":true}]};
+	const config = {"main":"index.js","info":{"name":"ClockPluginRedux","authors":[{"name":"NightWolf","discord_id":"242126810026868738","github_username":"NightWolf1038"}],"version":"1.1","description":"Adds a clock to Discord. Based on Jiiks' plugin","github":"https://github.com/NightWolf1038/BetterDiscord-Plugins/tree/master/ClockPluginRedux/","github_raw":"https://raw.githubusercontent.com/NightWolf1038/BetterDiscord-Plugins/master/ClockPluginRedux/ClockPluginRedux.plugin.js"},"changelog":[{"title":"What's New?","type":"improved","items":["Added hour color and background color"]}],"defaultConfig":[{"type":"switch","id":"format","name":"24 Hour Format","note":"Change the clock format.","value":true},{"type":"textbox","id":"hourColor","name":"Hour Color","note":"Change the color of the hour.","value":"#FFFFFF"},{"type":"textbox","id":"backColor","name":"Background Color","note":"Change the background color. RGBA values only.","value":"rgba(0,0,0,0)"}]};
 
 		return !global.ZeresPluginLibrary ? class {
 		getName() {return config.info.name;}
@@ -13,6 +13,8 @@ var ClockPluginRedux = (() => {
 		stop() {}
 	} : (([Plugin, Api]) => {
 		const plugin = (Plugin, Library) => {
+			
+			const {Logger, Patcher} = Library;
 
 	return class ClockPluginRedux extends Plugin {
 
@@ -73,6 +75,72 @@ var ClockPluginRedux = (() => {
 						this.interval = setInterval(function(){
 							self.ticktock12(self);
 						}, 1000);
+					}
+				}
+				if(id == "hourColor"){
+					var validHex = new RegExp('#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$');
+					if(value.match(validHex)){	
+						BdApi.clearCSS("clockPluginCss");
+						BdApi.injectCSS("clockPluginCss",`#clockPluginClock{
+								position:absolute;
+								left:45%;
+								right:50%;
+								margin-top:5px;
+								color:${value};
+								background-color:${this.settings.backColor};
+								padding:0 159px 0 15px;
+								min-width:40px;
+								max-width:55px;
+								z-index:100;
+							}`
+						);
+						clearInterval(this.interval);
+						if(this.settings.format == true){
+							this.ticktock();
+							this.interval = setInterval(function(){
+								self.ticktock(self);
+							}, 1000);
+						} else{
+							this.ticktock12();
+							this.interval = setInterval(function(){
+								self.ticktock12(self);
+							}, 1000);
+						}
+					} else{
+						BdApi.showToast("Invalid HEX Color", {type:"error", timeout:1500});
+					}
+				}
+				if(id == "backColor"){
+					var validRgba = new RegExp(/(rgb)a\((\d{1,3}%?,\s?){3}(1|0?\.\d+)\)/);
+					if(value.match(validRgba)){
+						BdApi.clearCSS("clockPluginCss");
+						BdApi.injectCSS("clockPluginCss",`#clockPluginClock{
+								position:absolute;
+								left:45%;
+								right:50%;
+								margin-top:5px;
+								color:${this.settings.hourColor};
+								background-color:${value};
+								padding:0 159px 0 15px;
+								min-width:40px;
+								max-width:55px;
+								z-index:100;
+							}`
+						);
+						clearInterval(this.interval);
+						if(this.settings.format == true){
+							this.ticktock();
+							this.interval = setInterval(function(){
+								self.ticktock(self);
+							}, 1000);
+						} else{
+							this.ticktock12();
+							this.interval = setInterval(function(){
+								self.ticktock12(self);
+							}, 1000);
+						}
+					} else{
+						BdApi.showToast("Invalid RGBA Color", {type:"error", timeout:1500});
 					}
 				}
 			});
